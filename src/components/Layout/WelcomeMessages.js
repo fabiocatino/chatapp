256a4bd4f27ee0dcf75ec53575623c69c1b44ba8
Fabio Catino <fabivs9@gmail.com>
2021-11-04 14:09:38 +0100

    first commit

---
+++ src/components/Layout/WelcomeMessages.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const HeaderMessage = () => {
+	return (
+		<div>
+			<div className="container  mx-auto text-center w-3/6 h-20 bg-green-100">
+				Get Started Create New Account
+			</div>
+		</div>
+	);
+};
+
+export const FooterMessage = () => {
+	return (
+		<>
+			<div className="container">Welcome</div>
+		</>
+	);
+};
